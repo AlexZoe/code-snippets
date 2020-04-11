5c152ed774665a92f955aee8dff88fea5a147f3e
Alexander Zoellner <zoellner.contact@gmail.com>
2020-04-11 21:07:33 +0900

    [feat] Add More Print Formats
    Implement methods for other formats such as hex and binary.
    
    Add example for forwarding format width to struct display method.

--- rust/print/printer.rs
+++ rust/print/printer.rs
@@ -21,6 +21,27 @@ impl fmt::Display for CoordinateSingleWrite {
     }
 }
 
+impl fmt::LowerHex for CoordinateSingleWrite {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{x: 0x{:x}, y: 0x{:x}}}", self.x, self.y)
+    }
+}
+
+impl fmt::UpperHex for CoordinateSingleWrite {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut width = 0;
+        if f.width().is_some() {
+            width = f.width().unwrap();
+        }
+        write!(f, "{{x: 0x{:0>width$X}, y: 0x{:0>width$X}}}", self.x, self.y, width = width)
+    }
+}
+
+impl fmt::Binary for CoordinateSingleWrite {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{x: {:b}, y: {:b}}}", self.x, self.y)
+    }
+}
 
 
 pub struct CoordinateMultiWrite {
@@ -49,7 +70,18 @@ impl fmt::Display for CoordinateMultiWrite {
 
 fn main () {
     let coord = CoordinateSingleWrite::new(3, 1);
-    println!("Coord: {}", coord);
+    println!("Decimal Coord: {}", coord);
+
+    let coord_lower_hex = CoordinateSingleWrite::new(13, 10);
+    println!("Hex Coord: {:x}", coord_lower_hex);
+
+    let coord_upper_hex = CoordinateSingleWrite::new(130, 10);
+    println!("Hex Coord: {:8X}", coord_upper_hex);
+
+    let coord_binary = CoordinateSingleWrite::new(13, 10);
+    println!("Binary Coord: {:b}", coord_binary);
+
     let coord2 = CoordinateMultiWrite::new(5, 7);
     println!("Coord: {}", coord2);
+    println!("x: {}, y: {}", coord2.x, coord2.y);
 }
