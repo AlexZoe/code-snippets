2c63c00a9b20a10dd0a305347be4a13901a63a58
Alexander Zoellner <zoellner.contact@gmail.com>
2020-04-16 21:19:11 +0900

    [feat] Add Templates to Vec Stack
    Use templates for using stack with different types.

--- rust/stack/vec_stack.rs
+++ rust/stack/vec_stack.rs
@@ -1,27 +1,28 @@
-pub struct Stack {
-    Items: Vec<i32>,
+pub struct Stack<T> {
+    items: Vec<T>,
 }
 
-impl Stack {
-    pub fn new() -> Stack {
+impl<T> Stack<T> {
+    pub fn new() -> Stack<T> {
         Stack {
-            Items: Vec::new(),
+            items: Vec::<T>::new(),
         }
     }
 
-    pub fn push(&mut self, data: i32) {
-        self.Items.push(data);
+    pub fn push(&mut self, data: T) {
+        self.items.push(data);
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
-        self.Items.pop()
+    pub fn pop(&mut self) -> Option<T> {
+        self.items.pop()
     }
 }
 
 fn main() {
     let mut my_stack = Stack::new();
-    my_stack.push(7);
-    my_stack.push(2);
+    my_stack.push("7");
+    my_stack.push("2");
     let item = my_stack.pop();
     println!("item: {}", item.unwrap().to_string());
+    println!("item: {}", my_stack.pop().unwrap().to_string());
 }
