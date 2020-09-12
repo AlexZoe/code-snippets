a5b0086b9f0dbd55a2ce9d1c0da3361f64b7ee22
Alexander Zoellner <zoellner.contact@gmail.com>
2020-09-12 22:41:18 +0900

    Add Generics Example

---
+++ rust/book/generics/src/main.rs
@@ -0,0 +1,17 @@
+// generic struct where the type for x and y is chosen separately
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+fn main() {
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+}
