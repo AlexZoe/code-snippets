f55d399c60d2192f4a2746beee3b5298c844530f
Alexander Zoellner <zoellner.contact@gmail.com>
2020-09-17 18:42:37 +0900

    Add Closure Example
    Closures are anonymous functions which can capture the environment
    in which they are declared.

---
+++ rust/book/closures/src/main.rs
@@ -0,0 +1,75 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    // closure: anonymous function
+    // this one takes no argument and returns i32
+    // closure gets assigned to variable
+    let calc_the_answer = || {
+        println!("compute the answer...");
+        42
+    };
+
+    println!("the answer is {}", calc_the_answer());
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+}
+
+
+// struct that has a field which implements the 'Fn' trait with one argument (u32)
+// and u32 return value
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
+        }
+    }
+}
