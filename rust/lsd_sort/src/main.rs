7a63aae36e07e1dd8407aefcc66c082567eded08
Alexander Zoellner <zoellner.contact@gmail.com>
2020-10-11 23:00:21 +0900

    [Rust] Finish LSD Sort Example

--- rust/lsd_sort/src/main.rs
+++ rust/lsd_sort/src/main.rs
@@ -6,7 +6,7 @@ fn sort(items: &mut Vec<String>, width: &usize) -> Vec<String> {
     let num_of_items = items.len();
     let mut tmp_items: Vec<String> = vec!(String::from(""); num_of_items);
 
-    for d in width-1 .. 0 {
+    for d in (0 .. width-1).rev() {
         let mut count: [u32; ALPHABET+1] = [0; ALPHABET+1];
 
         for i in 0 .. num_of_items {
@@ -17,8 +17,13 @@ fn sort(items: &mut Vec<String>, width: &usize) -> Vec<String> {
             count[(r+1) as usize] += count[r as usize];
         }
 
-        for i in 0 .. *width as u32 {
-            tmp_items[(count[items[i as usize].as_bytes()[d as usize] as usize] + 1) as usize] = items[i as usize].clone();
+        for i in 0 .. num_of_items as u32 {
+            tmp_items[(count[items[i as usize].as_bytes()[d as usize] as usize]) as usize] = items[i as usize].clone();
+            count[items[i as usize].as_bytes()[d as usize] as usize] += 1;
+        }
+
+        for i in 0 .. num_of_items as u32 {
+            items[i as usize] = tmp_items[i as usize].clone();
         }
     }
 
@@ -27,17 +32,24 @@ fn sort(items: &mut Vec<String>, width: &usize) -> Vec<String> {
 
 fn main() {
     let mut items: Vec<String> = env::args().collect();
-    println!("got {} items", items.len());
+    items.remove(0);
+    println!("Got {} items", items.len());
+
+    if items.len() == 0 {
+        return;
+    }
 
     let width = items[0].len();
 
+    println!("Items in received order");
     for i in &items {
         println!("{}", i);
     }
 
     let sorted_items = sort(&mut items, &width);
 
-    for i in sorted_items {
+    println!("\nItems in sorted order");
+    for i in &sorted_items {
         println!("{}", i);
     }
 }
