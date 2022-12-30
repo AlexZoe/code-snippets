bd70d53843aae84b673be8094faffefabb319b9f
Alexander Zoellner <zoellner.contact@gmail.com>
2022-12-30 22:21:36 +0900

    [Rust] Add Prime Factors

---
+++ rust/proj_euler/src/bin/problem_3.rs
@@ -0,0 +1,51 @@
+struct PrimeFactorizer {
+    input: u64,
+    next_candidate: u64,
+}
+
+impl PrimeFactorizer {
+    fn new(input: u64) -> Self {
+        PrimeFactorizer {
+            input,
+            next_candidate: 2,
+        }
+    }
+}
+
+impl Iterator for PrimeFactorizer {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.next_candidate < ((self.input as f64).sqrt() as u64) {
+            let mut found: u64 = 0;
+            while self.input % self.next_candidate == 0 {
+                found = self.next_candidate;
+                self.input /= found;
+            }
+
+            self.next_candidate += if self.next_candidate == 2 { 1 } else { 2 };
+
+            if found != 0 {
+                return Some(found);
+            }
+        }
+
+        let found = match self.input {
+            0 | 1 => None,
+            _ => Some(self.input),
+        };
+        self.input = 0;
+
+        found
+    }
+}
+
+fn main() {
+    let factorizer = PrimeFactorizer::new(600851475143);
+    let factors: Vec<u64> = factorizer.into_iter().collect();
+    println!("factors: {:?}\n", factors);
+
+    let factorizer = PrimeFactorizer::new(13195);
+    let factors: Vec<u64> = factorizer.into_iter().collect();
+    println!("factors: {:?}\n", factors);
+}
